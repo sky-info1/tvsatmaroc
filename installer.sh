1d76eef1f3e5fa86f16d584d0cba1b853bbfc074
sky-info1 <yassin.griche28@gmail.com>
2025-08-07 02:21:55 +0000

    installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Installing TVSATMAROC plugin..."
 cd /tmp
-wget https://github.com/sky-info1/tvsatmaroc/raw/main/TVSATMAROC_V1.70.zip -O tvsatmaroc.zip
+wget https://github.com/sky-info1/tvsatmaroc/raw/main/TVSATMAROC_V1.74.zip -O tvsatmaroc.zip
 unzip -o tvsatmaroc.zip -d /tmp/tvsatmaroc
 cp -r /tmp/tvsatmaroc/TVSATMAROC /usr/lib/enigma2/python/Plugins/Extensions/
 rm -rf /tmp/tvsatmaroc
